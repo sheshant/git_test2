5d732f61a27c8bccdb416e635293dc48028f72f5
sheshant <ssheshant@gmail.com>
2018-08-23 19:35:01 +0530

    added an admin screen for all the tasks to be created

--- urban_pipper/to_do_application/models.py
+++ urban_pipper/to_do_application/models.py
@@ -2,3 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+status_choices = (
+    ('PENDING', 'pending'),
+    ('COMPLETED', 'completed'),
+)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=1000, unique=True)
+    description = models.CharField(max_length=1000, unique=True)
+    status = models.CharField(choices=status_choices, default='pending', max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    number_of_hours_before_alert = models.IntegerField()
+    due_date = models.DateTimeField()
+
+
+
